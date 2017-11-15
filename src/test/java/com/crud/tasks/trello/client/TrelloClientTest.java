ade3a5fb8158502c80d5a342e9d23a1d8c4850b6
RadekKaniecki <radoslawkaniecki@gmail.com>
2017-11-15 19:30:00 +0100

    First test

---
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -0,0 +1,56 @@
+package com.crud.tasks.trello.client;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.config.TrelloConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrelloClientTest {
+
+    @InjectMocks
+    private TrelloClient trelloClient;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private TrelloConfig trelloConfig;
+
+    @Test
+    public void shouldFetchTrelloBoards() throws URISyntaxException {
+        //Given
+        when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+        when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+        when(trelloConfig.getTrelloAppToken()).thenReturn("test");
+
+        TrelloBoardDto[] trelloBoard = new TrelloBoardDto[1];
+        trelloBoard[0] = new TrelloBoardDto("test_board", "test_id", new ArrayList<>());
+
+        URI uri = new URI("http://test.com/members/radek397/boards?key=test&token=test&fields=name,id&lists=all");
+
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoard);
+
+        //When
+        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
+
+        //Then
+        assertEquals(1, fetchedTrelloBoards.size());
+        assertEquals("test_board", fetchedTrelloBoards.get(0).getName());
+        assertEquals("test_id", fetchedTrelloBoards.get(0).getId());
+        assertEquals(new ArrayList<>(), fetchedTrelloBoards.get(0).getLists());
+    }
+
+}
